32324053bc8fb98c9dd129036c7c8447e0ec1414
fenghaojiang <457420285@qq.com>
2020-10-21 18:23:10 +0800

    add element config

---
+++ vue/src/ApiData.vue
@@ -0,0 +1,24 @@
+<template>
+  <el-table>
+
+  </el-table>
+</template>
+
+<script>
+export default {
+name: "ApiData"
+  data () {
+  return
+  }
+}
+</script>
+
+<style>
+.el-table .warning-row {
+  background: oldlace;
+}
+
+.el-table .success-row {
+  background: #f0f9eb;
+}
+</style>
