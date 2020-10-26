5ade6d94f9d2a3f0ea3468973cdc28e5b54dddc6
fenghaojiang <457420285@qq.com>
2020-10-26 23:15:57 +0800

    version-1.0 finish

--- vue-tool/src/App.vue
+++ vue-tool/src/App.vue
@@ -2,18 +2,6 @@
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Vue-Books-Tool"/>
   <BooksList>
-    <el-table :data="this.booksData">
-      <el-table-column prop="Id" label="Id"></el-table-column>
-      <el-table-column prop="Title" label="书名"></el-table-column>
-      <el-table-column prop="Author" label="作者"></el-table-column>
-      <el-table-column prop="Translator" label="译者"></el-table-column>
-      <el-table-column prop="Press" label="出版社"></el-table-column>
-      <el-table-column prop="Date" label="日期"></el-table-column>
-      <el-table-column prop="Price" label="价格"></el-table-column>
-      <el-table-column prop="Star" label="星级"></el-table-column>
-      <el-table-column prop="Comment" label="评价人数"></el-table-column>
-      <el-table-column prop="Quote" label="引言"></el-table-column>
-    </el-table>
   </BooksList>
 </template>
 
@@ -24,14 +12,14 @@ import BooksList from "@/components/BooksList";
 
 export default {
   name: 'App',
+  components: {
+    HelloWorld,
+    BooksList,
+  },
   data() {
     return {
       booksData:BooksList.data().booksData
     }
-  },
-  components: {
-    HelloWorld,
-    BooksList,
   },
 }
 </script>
--- vue-tool/src/components/BooksList.vue
+++ vue-tool/src/components/BooksList.vue
@@ -1,6 +1,35 @@
 <template>
   <div class="books-data" :data="booksData">
-    {{booksData}}
+    <table>
+      <thead>
+      <tr>
+        <td>Id</td>
+        <td>Title</td>
+        <td>Author</td>
+        <td>Translator</td>
+        <td>Press</td>
+        <td>Date</td>
+        <td>Price</td>
+        <td>Star</td>
+        <td>Comment</td>
+        <td>Quote</td>
+      </tr>
+      </thead>
+      <tbody id="book-body">
+      <tr v-for="books in booksData" :key="books">
+        <td>{{books.Id}}</td>
+        <td>{{books.Title}}</td>
+        <td>{{books.Author}}</td>
+        <td>{{books.Translator}}</td>
+        <td>{{books.Press}}</td>
+        <td>{{books.Date}}</td>
+        <td>{{books.Price}}</td>
+        <td>{{books.Star}}</td>
+        <td>{{books.Comment}}</td>
+        <td>{{books.Quote}}</td>
+      </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
